778372103531ffe2b26c85c2e077f9ca5114fbad
douglasfonseca22 <douglasfonseca2010@hotmail.com>
2021-05-31 19:39:57 -0300

    Primeiro commit

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Routes from './src/routes';
+import {
+  useFonts,
+  Jost_400Regular,
+  Jost_600SemiBold
+} from '@expo-google-fonts/jost'
+import AppLoading from 'expo-app-loading'
+
+export default function App() {
+  const [ fontsLoaded] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold
+  });
+
+  if(!fontsLoaded)
+  return <AppLoading />
+  return (
+    <Routes  />
+  )
+}
