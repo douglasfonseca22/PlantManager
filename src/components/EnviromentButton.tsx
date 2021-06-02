a59542affad3248765d193a3787632437ca59e52
Douglas Fonseca <douglasfonseca2010@hotmail.com>
2021-06-01 22:39:50 -0300

    Update EnviromentButton.tsx
    Correção color componente

--- src/components/EnviromentButton.tsx
+++ src/components/EnviromentButton.tsx
@@ -43,7 +43,7 @@ const styles = StyleSheet.create({
         marginHorizontal: 5
     },
     containerActive: {
-        backgroundColor: colors.green_light
+        backgroundColor: colors.green
     },
     text: {
         color: colors.heading,
